53e068a90dc65884921a7aba9e712be7abb609b4
Pritam Lad <pritamlad18@gmail.com>
2022-01-10 16:50:03 +0530

    simple go service

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r*http.Request) {
 		fmt.Fprint(rw, "Hello World\n")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != null {
+			http.Error(rw, "Ooops", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw,"Hello %s ", d)
 	})
 	http.ListenAndServe(":9090",nil)
 }
